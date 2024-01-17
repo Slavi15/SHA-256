bc13379c01edd34efe5c8083484c43439836c325
Slavi15 <slavirezashkiofficial@gmail.com>
2024-01-18 00:54:53 +0200

    Add comment block on top of the main CPP file

--- SHA-256/SHA256.cpp
+++ SHA-256/SHA256.cpp
@@ -1,3 +1,16 @@
+/**
+*
+* Solution to course project # 6
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2023/2024
+*
+* @author Slavi Rezashki
+* @idnumber 6MI0600339
+* @compiler MSVC
+*
+*/
+
 #include <iostream>
 #include <fstream>
 
